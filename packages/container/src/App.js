484487363f854455bce50d42f548eac8316a065c
Sudo-BangBang <s.j.tay@outlook.com>
2021-01-04 16:25:49 +0000

    added login/logout

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,7 +1,8 @@
-import React, {lazy, Suspense} from 'react';
+import React, {lazy, Suspense, useState} from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 
+import Progress from './components/Progress'
 import Header from './components/Header'
 
 
@@ -15,15 +16,18 @@ const generateClassName = createGenerateClassName({
 
 
 export default () => {
+  const [isSignedIn, setIsSignedIn] = useState(false);
   return (
     <BrowserRouter>
       <StylesProvider generateClassName={generateClassName}>
         
           <div>
-            <Header />
-            <Suspense fallback={<div>Loading...</div>}>
+            <Header isSignedIn={isSignedIn} onSignOut={()=>setIsSignedIn(false)} />
+            <Suspense fallback={<Progress/>}>
               <Switch>
-                <Route path="/auth" component={AuthLazy} />
+                <Route path="/auth">
+                    <AuthLazy onSignIn={() => setIsSignedIn(true)} />
+                </Route>
                 <Route path="/" component={MarketingLazy} />
               </Switch>
             </Suspense>
